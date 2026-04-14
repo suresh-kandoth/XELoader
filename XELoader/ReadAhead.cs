b5522e10941afe6025da0bbda5b0a9b8853efaa1
moraja <moraja@microsoft.com>
2026-04-14 15:13:43 +0300

    Migrate XELoader from .NET Framework 4.8.1 to .NET 9
    - Convert project from legacy .csproj to SDK-style targeting net9.0
    - Replace System.Data.SqlClient with Microsoft.Data.SqlClient 6.0.2
    - Upgrade XEvent SDK from 2022 to 2025 (v17) with XE.Core + XEvent.Linq
    - Add -T parameter to trust server SSL certificates (self-signed certs)
    - Add -E parameter to disable SQL connection encryption
    - Add -V parameter for verbose output with full exception stack traces
    - Improve -p pattern matching with regex-based wildcard support (* and ?)
    - Add descriptive error message with workarounds when file name format is unexpected
    - Catch SSL certificate errors with actionable guidance for resolution
    - Remove legacy files: App.config, packages.config, FakesAssemblies

--- XELoader/ReadAhead.cs
+++ XELoader/ReadAhead.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 using System.IO;
@@ -26,35 +26,22 @@ namespace XELoader
 
         public void DoReadAhead()
         {
-            try
+            using (FileStream s = new FileStream(m_File_To_Read, FileMode.Open, FileAccess.Read, FileShare.Read, m_ReadAheadChunk, FileOptions.SequentialScan))
             {
-                using (FileStream s = new FileStream(m_File_To_Read, FileMode.Open, FileAccess.Read, FileShare.Read, m_ReadAheadChunk, FileOptions.SequentialScan))
+                byte[] DataBuffer = new byte[m_ReadAheadChunk];
+
+                int Loops = 0;
+                int BytesRead = s.Read(DataBuffer, 0, m_ReadAheadChunk);
+                while (BytesRead > 0)
                 {
-                    byte[] DataBuffer = new byte[m_ReadAheadChunk];
-
-                    int Loops = 0;
-                    int BytesRead = s.Read(DataBuffer, 0, m_ReadAheadChunk);
-                    while (BytesRead > 0)
+                    BytesRead = 0;
+                    Loops++;
+                    if (0 == Loops % 10)
                     {
-                        BytesRead = 0;
-                        Loops++;
-                        if (0 == Loops % 10)
-                        {
-                            Thread.Sleep(1);
-                        }
-                        BytesRead = s.Read(DataBuffer, 0, m_ReadAheadChunk);
+                        Thread.Sleep(1);
                     }
+                    BytesRead = s.Read(DataBuffer, 0, m_ReadAheadChunk);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Thread {0} : ***  Exception encountered in DoReadAhead routine *** ", Thread.CurrentThread.ManagedThreadId);
-                Console.WriteLine("Thread {0} : Exception message : {1}  ", Thread.CurrentThread.ManagedThreadId, e.Message);
-                Console.WriteLine("Thread {0} : Exception code : {1}  ", Thread.CurrentThread.ManagedThreadId, System.Runtime.InteropServices.Marshal.GetHRForException(e));  //Using e.HResult gives CS0122 in new versions of .NET
-                // Console.WriteLine("Thread {0} : Exception stack : {1}  ", Thread.CurrentThread.ManagedThreadId, e.StackTrace);
-                Console.ResetColor();
-
             }
         }
     }
--- XELoader/TrackStatus.cs
+++ XELoader/TrackStatus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 using System.IO;
